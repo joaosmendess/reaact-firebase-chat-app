1a5f23d3fa01f8f2a363456ec70167910565b73c
Joao Mendes <joaosilva0721@gmail.com>
2023-07-27 06:54:58 -0300

    Configurando Firebase

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD92fYAuawBw8EV3Fec4yfLsCLPSQExM0w",
+  authDomain: "chat-app-72343.firebaseapp.com",
+  projectId: "chat-app-72343",
+  storageBucket: "chat-app-72343.appspot.com",
+  messagingSenderId: "211905584564",
+  appId: "1:211905584564:web:5be2dbc2454f411bbe66e1"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
