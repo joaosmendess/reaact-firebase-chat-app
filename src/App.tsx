e1f87a8d510d5c8bef6fbd01c4953a9a8ca6b346
Joao Mendes <joaosilva0721@gmail.com>
2023-07-27 06:17:29 -0300

    Iniciando projeto com vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1>Hello World!</h1>
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestias sed optio officia porro. Amet molestias ducimus, omnis necessitatibus adipisci quia ea maxime rem vitae? Dolores aperiam doloribus repellendus sequi.</p>
+   
+      </div>
+     
+    </>
+  )
+}
+
+export default App
